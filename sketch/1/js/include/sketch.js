5f32bec5f071d992545154fa63aefecb50ad30a0
hokuto-kato <hokuto15@gmail.com>
2022-04-19 19:05:52 +0900

    #1

---
+++ sketch/1/js/include/sketch.js
@@ -0,0 +1,19 @@
+import "./globals"
+
+const target = document.querySelector("[data-target]")
+export default function() {
+	const sketch = (p) => {
+		p.setup = () => {
+			p.createCanvas(600, 600)
+			p.background("#28020d")
+			p.noCursor()
+		}
+
+		p.draw = () => {
+			p.stroke("#28020d")
+			p.fill("#629e62")
+			p.circle(p.mouseX, p.mouseY, p.random(100))
+		}
+	}
+	new p5(sketch, target)
+}
