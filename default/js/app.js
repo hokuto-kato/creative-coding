f90461a635ad579bf60e719c044dd8087d30f045
hokuto-kato <hokuto15@gmail.com>
2022-04-26 15:49:02 +0900

    webfontのためのモジュール

--- default/js/app.js
+++ default/js/app.js
@@ -1,6 +1,8 @@
 import "../sass/app.sass"
 import sketch from './include/sketch'
+import webFont from "./include/webFont"
 
 document.addEventListener("DOMContentLoaded", () => {
 	sketch()
+	webFont()
 })
